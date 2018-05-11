ba6d098f13cafff16b9506c67f0cc3ffb16abd16
Israel Gomez <imarban@MacBook-Air-de-Tiare.local>
2018-05-11 13:09:41 -0500

    Add missing Transaction Id

--- src/main/java/com/imarban/app/model/Transaction.java
+++ src/main/java/com/imarban/app/model/Transaction.java
@@ -1,10 +1,34 @@
 package com.imarban.app.model;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Transaction {
+
+    private UUID id;
+
     private Double amount;
     private String description;
     private LocalDate date;
     private Integer userId;
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public Double getAmount(){
+        return this.amount;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
+    public Integer getUserId(){
+        return this.userId;
+    }
 }
