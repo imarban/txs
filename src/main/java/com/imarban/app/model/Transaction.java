8611ea041b94b76257a8f701ae056697c0aee2b9
Israel Gomez <imarban@MacBook-Air-de-Tiare.local>
2018-05-11 12:52:32 -0500

    Add Transaction model

---
+++ src/main/java/com/imarban/app/model/Transaction.java
@@ -0,0 +1,10 @@
+package com.imarban.app.model;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private Double amount;
+    private String description;
+    private LocalDate date;
+    private Integer userId;
+}
