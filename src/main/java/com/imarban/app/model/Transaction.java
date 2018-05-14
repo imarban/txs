de8b502118b8764d2a11f8dde5b0f49f94efc7f9
imarban <imarban22@gmail.com>
2018-05-13 22:54:49 -0500

    Add Jackson dependency and Json annotations

--- src/main/java/com/imarban/app/model/Transaction.java
+++ src/main/java/com/imarban/app/model/Transaction.java
@@ -1,16 +1,28 @@
 package com.imarban.app.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public class Transaction {
+public class Transaction implements Serializable {
 
     private UUID id;
 
     private Double amount;
     private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
+
+    @JsonProperty("user_id")
     private Integer userId;
+
+    public Transaction() {
+        id = UUID.randomUUID();
+    }
 
     public UUID getId() {
         return this.id;
